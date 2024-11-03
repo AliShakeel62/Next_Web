eccd381fc9d3c8d82a7fc406399b3b94b10feadd
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-11-03 21:16:59 +0500

    bug:Css issue fixed

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Mainboard from "./Components/mainbord/page";
+import NavBar from "./navbar/page";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <Mainboard />
+    </>
+  );
+}
